c4693ad09bf9d6b3bc202ff207e2bbcd79106cbf
Lucas Jellema <lucas.jellema@amis.nl>
2020-01-15 13:25:10 +0100

    RSS Feeder

---
+++ functions/rss-feeder/oci-configuration.js
@@ -0,0 +1,19 @@
+const configs=
+    {
+        namespaceName: 'idtwlqf2hanz',
+        region: 'us-ashburn-1',
+        pass_phrase: "passphrase123#$",
+        authUserId: "ocid1.user.oc1..aaaaaaaas5gzekquyfcvply3nscvstactfnhznlxphn3tyvn6gmkemlmxtdq",
+        identityDomain: "identity.us-ashburn-1.oraclecloud.com",
+        compartmentId: 'ocid1.compartment.oc1..aaaaaaaatxf2nfi7prglkhntadfj4tuxlfms36xhqc4hekuif6wjnoyq4ilq', //functions-compartment
+        tenancyId: "ocid1.tenancy.oc1..aaaaaaaag7c7slwmlvsodyym662ixlsonnihko2igwpjwwe2egmlf3gg6okq",
+        keyFingerprint: "33:5f:59:02:07:18:d5:41:a6:db:29:3c:2e:71:0a:62",
+        privateKeyPath: "./oci_api_key.pem",
+        coreServicesDomain: "iaas.us-ashburn-1.oraclecloud.com",
+        streamingAPIEndpoint: "streaming.us-ashburn-1.oci.oraclecloud.com" 
+    }
+
+module.exports = {
+    configs : configs
+};
+
