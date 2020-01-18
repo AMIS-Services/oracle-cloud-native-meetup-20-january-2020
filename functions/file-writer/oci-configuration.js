b678b647ccdf3ff86171703ecbb4e0c5da461fc8
Lucas Jellema <lucas.jellema@amis.nl>
2020-01-18 09:02:24 +0100

    .

--- functions/file-writer/oci-configuration.js
+++ functions/file-writer/oci-configuration.js
@@ -1,18 +1,6 @@
 const configs=
     {
-        namespaceName: 'idtwhanz',
-        region: 'us-ashburn-1',
-        compartmentId: 'ocid1.compartment.oc1..aaaaaaaatxwjnoyq4ilq', //functions-compartment
-        pass_phrase: "passphrase123#$",
-        authUserId: "ocid1.user.oc1..aaaaaaamlmxtdq",
-        identityDomain: "identity.us-ashburn-1.oraclecloud.com",
-        tenancyId: "ocid1.tenancy.oc1..aaaaaagg6okq",
-        keyFingerprint: "33:5f:59:02:07:18:d5:62",
-        privateKeyPath: "./oci_api_key.pem",
-        coreServicesDomain: "iaas.us-ashburn-1.oraclecloud.com",
-        bucketOCID: process.env['bucketOCID'],
-        bucketName:process.env['bucketName'],
-        objectStorageAPIEndpoint:"objectstorage.us-ashburn-1.oraclecloud.com"
+        todo: "Replace with Proper Configuration"
     }
 
 module.exports = {
