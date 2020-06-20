9e266349c613aaa83800f4d4ec0ee22ff962c719
Lucas Jellema <lucas.jellema@amis.nl>
2020-06-20 07:45:01 +0200

    refer to environment variables in oci-configuration.js

--- functions/file-writer/oci-configuration.js
+++ functions/file-writer/oci-configuration.js
@@ -1,9 +1,20 @@
 const configs=
     {
-        todo: "Replace with Proper Configuration"
+        namespaceName: `${process.env["ns"]}`,
+        region: `${process.env["REGION"]}`,
+        compartmentId: `${process.env["compartmentId"]}`, //lab-compartment
+        authUserId: `${process.env["USER_OCID"]}`,
+        identityDomain: `identity.${process.env["REGION"]}.oraclecloud.com`,
+        tenancyId: `${process.env["TENANCY_OCID"]}`,
+        keyFingerprint: "YOUR_FINGERPRINT_FROM FILE ./oci_api_key.pem",
+        privateKeyPath: "./oci_api_key.pem",
+        coreServicesDomain: `iaas.${process.env["REGION"]}.oraclecloud.com`,
+        bucketOCID: process.env['bucketOCID'],
+        bucketName:process.env['bucketName'],
+        objectStorageAPIEndpoint:`objectstorage.${process.env["REGION"]}.oraclecloud.com`,
+        streamingAPIEndpoint: `streaming.${process.env["REGION"]}.oci.oraclecloud.com` 
     }
 
-module.exports = {
+ module.exports = {
     configs : configs
 };
-
